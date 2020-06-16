b86aadaf1ef20e5bf6c52836d3f3347ac69ee123
iKKhalid <K.Khalid@outlook.com>
2020-06-16 07:37:52 +0100

    Quote Update

--- get-quote.py
+++ get-quote.py
@@ -4,14 +4,21 @@ import random
 def main():
     print("Keep it logically awesome.")
 
+    f = open('quotes.txt', 'a')
+    f.write('Hello\n')
+    f.close()
+
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
 
-    last = 13
-    rnd = random.randint(0, last)
+    last01 = 13
+    last02 = 13
+    rnd01 = random.randint(0, last01)
+    rnd02 = random.randint(0, last02)
 
-    print(quotes[rnd])
+    print(quotes[rnd01].rstrip())
+    print(quotes[rnd02])
 
 
 if __name__ == "__main__":
